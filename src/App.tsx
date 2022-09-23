0d712f17664e4b89062f98c2c9465b653abd0527
quanth <quanthph14921@fpt.edu.vn>
2022-09-23 09:18:37 +0700

    [Client] feat: Create UI page checkout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { ClientLayout } from './layouts';
@@ -6,6 +7,8 @@ import Signup from './pages/Signup';
 import DetailProduct from "./pages/Detail";
 import ContactPage from "./pages/Contact";
 import HomePage from './pages/Home'
+import CheckoutPage from "./pages/Checkout";
+
 
 function App() {
   return (
@@ -50,6 +53,14 @@ function App() {
               }
           ></Route>
 
+        <Route
+          path="/checkout"
+          element={
+            <ClientLayout>
+              <CheckoutPage />
+            </ClientLayout>
+          }
+        ></Route>
       </Routes>
     </>
   );
