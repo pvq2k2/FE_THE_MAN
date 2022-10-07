d2d5f9309db0a0eb6fbf51f61066d1514678a240
pvq2k2 <vanquyet2392002e@gmail.com>
2022-10-07 15:41:20 +0700

    feat: Create Function Signup - Create api client auth - Create type user - Create auth slice

--- src/redux/rootReducer.tsx
+++ src/redux/rootReducer.tsx
@@ -4,8 +4,10 @@ import postReducer from "./slices/postSlice";
 import cateReducer from "./slices/catePostSlice"
 import cateproReducer from "./slices/cateProductSlice";
 import productReducer from "./slices/productSlice";
+import authReducer from "./slices/authSlice";
 
 const rootReducer = combineReducers({
+  auth: authReducer,
   post: postReducer,
   catePro : cateproReducer,
   catepost: cateReducer,
