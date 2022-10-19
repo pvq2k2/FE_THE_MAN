2edf96be9bfbc6907d6a2016f0e675c2cde2b42f
caotruong0511 <truongcmph15429@fpt.edu.vn>
2022-10-20 00:46:45 +0700

    [Client] axios bearer token

--- src/api-cilent/Config.tsx
+++ src/api-cilent/Config.tsx
@@ -2,5 +2,38 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "http://localhost:8000",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
+const { token } = JSON.parse(localStorage.getItem("user") as string);
+
+instance.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+// Add a request interceptor
+instance.interceptors.request.use(
+  function (config) {
+    if (token) {
+      config.headers = {
+        Authorization: `Bearer ${token}`,
+      };
+    }
+
+    // Do something before request is sent
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+
 export default instance;
