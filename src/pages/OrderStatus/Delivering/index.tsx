f4fff004a86371fbff433c74dcc7ea229ab5a46e
daongoclinh22 <daongoclinh22@gmail.com>
2022-10-21 02:03:44 +0700

    feat: create UI OrderStatus

---
+++ src/pages/OrderStatus/Delivering/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Deliver = () => {
+  return (
+    <div>
+      <b>Dang thuc hien giao hang</b>
+    </div>
+  )
+}
+
+export default Deliver
---
+++ src/pages/OrderStatus/Waitting/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Waitting = () => {
+  return (
+    <div className='bg-pink-500 w-[200px] h-6'>
+      linh
+    </div>
+  )
+}
+
+export default Waitting
