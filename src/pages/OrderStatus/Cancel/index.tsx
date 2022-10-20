3e76cfe2aabc976080744af51cdeb537ed4f8d92
daongoclinh22 <daongoclinh22@gmail.com>
2022-10-21 02:03:44 +0700

    feat: create UI OrderStatus

---
+++ src/pages/OrderStatus/Cancel/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cancel = () => {
+  return (
+    <div>
+      <b>Da huy don hang</b>
+    </div>
+  )
+}
+
+export default Cancel
---
+++ src/pages/OrderStatus/Done/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Done = () => {
+  return (
+    <div>
+      <b>Da giao hang thanh cong</b>
+    </div>
+  )
+}
+
+export default Done
