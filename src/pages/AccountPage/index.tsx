8870d394f87013248608cdd306f1a7c66ac11dea
QuanTran14921 <tquan7965@gmail.com>
2022-11-16 12:36:57 +0700

    Render : Account page

--- src/pages/AccountPage/index.tsx
+++ src/pages/AccountPage/index.tsx
@@ -1,15 +1,26 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { User } from '../../models/User';
 
 type Props = {}
 
 const AccountPage = (props: Props) => {
+
+  let isLogged = useSelector((state: any) => state.auth.isLogged);
+  let currentUser = useSelector((state: any) => state.auth.currentUser) as User;
+  if (localStorage.getItem("user")) {
+    (isLogged = true),
+      (currentUser = JSON.parse(localStorage.getItem("user") || "{}"));
+  }
+  console.log(currentUser);
+  
   return (
     <div className='p-40'>
         <section className="grid grid-cols-2 grid-flow-row-dense gap-2">
         <div className="logo flex gap-5 items-center">
-            <img src="https://i.pinimg.com/736x/56/86/03/568603cbd1860c67bf8f6776cbe7f885.jpg" className="bg-left-top bg-no-repeat h-40 w-40" alt="" />
+            <img src={`${currentUser?.users?.fullname}`} className="bg-left-top bg-no-repeat h-40 w-40" alt="" />
             <div>
-                <h4 className="font-bold text-lg">truongcmph15429@fpt.edu.vn</h4>
+                <h4 className="font-bold text-lg">{currentUser?.users?.email}</h4>
                 <button>Sửa Thông Tin Tài Khoản</button>
             </div>
             
@@ -19,9 +30,9 @@ const AccountPage = (props: Props) => {
 
             <div className='m-2'>
                 <div className='text-xl pb-2'>Email: </div>
-                <div className='text-lg pb-2'>truongcmph15429@fpt.edu.vn</div>
+                <div className='text-lg pb-2'>{currentUser?.users?.email}</div>
                 <div className='text-xl pb-2' >Họ Tên :</div>
-                <div className='text-lg pb-2'>truongcmph15429@fpt.edu.vn</div>
+                <div className='text-lg pb-2'>{currentUser?.users?.fullname}</div>
             </div>
           </div>
         </section>
