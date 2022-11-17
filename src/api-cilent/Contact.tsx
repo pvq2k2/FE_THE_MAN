40e2d6882e3309a6693e97a97412147a073e7bc0
pvq2k2 <vanquyet2392002@gmail.com>
2022-11-17 13:53:54 +0700

    [Client] Create contactSlice - refactor: API Contact - Add reducer contact

--- src/api-cilent/Contact.tsx
+++ src/api-cilent/Contact.tsx
@@ -1,20 +1,20 @@
 import { Contact } from "../models/Contact";
 import instance from "./Config";
-export const getAllContact = (): Promise<Contact[]> => {
+export const getAll = (): Promise<Contact[]> => {
   return instance.get("/contacts");
 };
-export const removeContact = (id: string): Promise<Contact> => {
+export const remove = (id: string): Promise<Contact> => {
   return instance.delete(`/contact/${id}`);
 };
 
-export const addContact = (contact: Contact): Promise<Contact> => {
+export const add = (contact: Contact): Promise<Contact> => {
   return instance.post("/contact", contact);
 };
 
-export const getContact = (id: string): Promise<Contact> => {
+export const read = (id: string): Promise<Contact> => {
   return instance.get(`/contact/${id}`);
 };
 
-export const updateContact = (contact: Contact): Promise<Contact> => {
+export const update = (contact: Contact): Promise<Contact> => {
   return instance.put(`/contact/${contact._id}`, contact);
 };
