33b8ddd0d30fb0f11bca07647aa680076227dde2
QuanTran14921 <tquan7965@gmail.com>
2022-11-19 21:52:29 +0700

    update ul account page

--- src/pages/AccountPage/Editaccount.tsx
+++ src/pages/AccountPage/Editaccount.tsx
@@ -7,14 +7,19 @@ import { toast } from 'react-toastify';
 import { User } from '../../models/User';
 import { getUser, updateUser } from '../../redux/slices/userSlice';
 import { RootState, useAppDispatch } from '../../redux/store';
+import styles from "./Contact.module.css";
 type Props = {}
 type Inputs = {
-    fullname: string;
-    email: string;
-    status: string;
+  fullname: string;
+  phone: string;
+  address: string;
+  email: string;
+  image: string;
+  status: string
   };
 const Editaccount = (props: Props) => {
     const dispatch = useAppDispatch();
+    const [preview, setPreview] = useState<string>();
     const users = useSelector((state: RootState) => state.user);
     const [status, setStatus] = useState(users.User.status);
     const navigate = useNavigate();
@@ -74,117 +79,143 @@ const Editaccount = (props: Props) => {
   
 
   return (
-    <div className='p-40'>
-        <section className="grid grid-cols-2 grid-flow-row-dense gap-2">
-        <div className="logo flex gap-5 items-center">
-            <img src={`${currentUser?.users?.fullname}`} className="bg-left-top bg-no-repeat h-40 w-40" alt="" />
-            <div>
-                <h4 className="font-bold text-lg">{currentUser?.users?.email}</h4>
-                <Link to={`/account`} className="sm:ml-3">
-                <button>Quay lại</button>
-        </Link>
+    <div>
+      <div className={styles.form_container}>
+        <h2 className={styles.title}>Thông tin người dùng</h2>
+        <form className={styles.form_contact} onSubmit={handleSubmit(onSubmit)}>
+          <div className={styles.form_row}>
+            <div className={styles.form_group}>
+              <label htmlFor="" className={styles.text_label}>
+                Tên người dùng
+              </label>
+              <input
+                className={`${errors.fullname && styles.input_error}`}
+                {...register("fullname", {
+                  required: "Vui lòng nhập họ và tên",
+                  minLength: {
+                    value: 5,
+                    message: "Vui lòng nhập đầy đủ họ tên",
+                  },
+                })}
+                type="text"
+                placeholder="Tên của bạn"
+              />
+              <p className={styles.error}>{errors.fullname?.message}</p>
             </div>
             
           </div>
-          <div className='pl-20'>
-            <h1 className='text-4xl'>Sửa Thông Tin Người Dùng</h1>
-
-            <div className='m-2'>
-            <form action="" method="POST" onSubmit={handleSubmit(onSubmit)}>
-              <div className="shadow sm:rounded-md sm:overflow-hidden">
-                <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
-                  <div>
-                    <label
-                      htmlFor="fullname"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      Họ Tên
-                    </label>
-                    <div className="mt-1">
-                      <input
-                        type="text"
-                        placeholder="Họ tên*"
-                        {...register("fullname", {
-                          required: "Vui lòng nhập họ tên",
-                        })}
-                        id="name-add-product"
-                        className="shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md p-2"
-                      />
-                      <div className="text-sm mt-0.5 text-red-500">
-                        {errors.fullname?.message}
-                      </div>
-                    </div>
-                  </div>
-                  <div>
-                    <label
-                      htmlFor="fullname"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      Số Điện Thoại
-                    </label>
-                    <div className="mt-1">
-                      <input
-                        type="text"
-                        placeholder="Số Điện Thoại*"
-                        id="name-add-product"
-                        className="shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md p-2"
-                      />
-                      <div className="text-sm mt-0.5 text-red-500">
-                        {errors.fullname?.message}
-                      </div>
-                    </div>
-                  </div>
-                    
-
-                  <div>
-                    <label className="block text-sm font-medium text-gray-700">
-                      Hình ảnh
-                    </label>
-                    <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-                      <div className="space-y-1 text-center">
-                        <svg
-                          className="mx-auto h-12 w-12 text-gray-400"
-                          stroke="currentColor"
-                          fill="none"
-                          viewBox="0 0 48 48"
-                          aria-hidden="true"
-                        >
-                          <path
-                            d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                            strokeWidth={2}
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          />
-                        </svg>
-                        <div className="flex text-sm text-gray-600">
-                          <input
-                            
-                            id="file-upload"
-                            type="file"
-                          />
-                          <div className="text-sm mt-0.5 text-red-500">
-                           
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  
-                </div>
-                <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                  <button
-                    type="submit"
-                    className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                  >
-                    <HiOutlineCheck className="mr-2 text-[20px]" />
-                    Lưu
-                  </button>
-                </div>
-              </div>
-            </form>
+          <div className={styles.form_row}>
+            <div className={styles.form_group}>
+              <label htmlFor="" className={styles.text_label}>
+                Email
+              </label>
+              <input
+                className={`${errors.email && styles.input_error}`}
+                {...register("email", {
+                  required: "Vui lòng nhập email",
+                })}
+                type="text"
+                placeholder="Email của bạn"
+                disabled
+              />
+              <p className={styles.error}>{errors.email?.message}</p>
+            </div>
+            <div className={styles.form_group}>
+              <label htmlFor="" className={styles.text_label}>
+                Số điện thoại
+              </label>
+              <input
+                className={`${errors.phone && styles.input_error}`}
+                {...register("phone", {
+                  required: "Vui lòng nhập số điện thoại",
+                  pattern: {
+                    value: /(84|0[3|5|7|8|9])+([0-9]{8})\b/g,
+                    message: "Vui lòng nhập đúng định dạng số điện thoại",
+                  },
+                })}
+                type="text"
+                placeholder="Số điện thoại của bạn"
+              />
+              <p className={styles.error}>{errors.phone?.message}</p>
             </div>
           </div>
-        </section>
+          {/* <div className={styles.form_group}>
+            <textarea
+              className={`${styles.area} ${
+                errors.content && styles.input_error
+              }`}
+              defaultValue={""}
+              placeholder="Nội dung"
+              {...register("content", {
+                required: "Vui lòng nhập nội dung",
+              })}
+            />
+            <p className={styles.error}>{errors.content?.message}</p>
+          </div> */}
+          <div className={styles.form_row}>
+            <div>
+              <label htmlFor="" className={styles.text_label}>
+                Hình ảnh
+              </label>
+
+              <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+                <div className="space-y-1 text-center">
+                  <svg
+                    className="mx-auto h-12 w-12 text-gray-400"
+                    stroke="currentColor"
+                    fill="none"
+                    viewBox="0 0 48 48"
+                    aria-hidden="true"
+                  >
+                    <path
+                      d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                      strokeWidth={2}
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                  <div className="flex text-sm text-gray-600">
+                    <input
+                      {...register("image", {
+                        required: "Vui lòng chọn ảnh",
+                      })}
+                      onChange={(e: any) => {
+                        setPreview(URL.createObjectURL(e.target.files[0]));
+                      }}
+                      id="file-upload"
+                      type="file"
+                    />
+                    <div className="text-sm mt-0.5 text-red-500">
+                      {errors.image?.message}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="">
+              <label htmlFor="" className={styles.text_label}>
+                Ảnh xem trước
+              </label>
+
+              <div className="mt-1 w-40 h-40 relative">
+                <img
+                  src={
+                    preview ||
+                    "https://res.cloudinary.com/assignmentjs/image/upload/c_thumb,w_200,g_face/v1648723660/img/noimage_mzjwxl.png"
+                  }
+                  alt="Preview Image"
+                  className="h-40 w-40 rounded-sm object-cover"
+                />
+              </div>
+            </div>
+            {/* End image review */}
+
+            {/* Add image */}
+          </div>
+
+          <button className={styles.btn}>Cập nhật</button>
+        </form>
+      </div>
     </div>
   )
 }
