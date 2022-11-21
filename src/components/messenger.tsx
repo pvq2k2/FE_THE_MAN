f2c58bd65053a5fb1edc7b59ef87417b3b5020c5
linhngocdao <linhdaongoc22@gmail.com>
2022-11-21 11:42:08 +0700

    fix: role messaege and fix layout

--- src/components/messenger.tsx
+++ src/components/messenger.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { BsMessenger } from 'react-icons/bs'
+import { useSelector } from 'react-redux';
+import { User } from '../models/User';
 
 const messenger = () => {
+  let currentUser = useSelector((state: any) => state.auth.currentUser) as User;
   return (
     <div>
-        <p style={{
-                     position: 'fixed',
+      {currentUser?.users?.role == 0 ? (  <p style={{
+           position: 'fixed',
           fontSize: '34px',
           bottom: '80px',
           right: '30px',
@@ -14,7 +17,7 @@ const messenger = () => {
           color: 'rgb(10, 124, 255)',
           border: 'none',
         }}><BsMessenger/></p>
-        
+        ) : true}
     </div>
   )
 }
