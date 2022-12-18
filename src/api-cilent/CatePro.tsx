6af8c433a6a81066bf4be999490402e223fdc074
caotruong0511 <truongcmph15429@fpt.edu.vn>
2022-12-18 21:40:52 +0700

    fix cateProduct add fiend status

--- src/api-cilent/CatePro.tsx
+++ src/api-cilent/CatePro.tsx
@@ -1,28 +1,30 @@
 import { ICatePro } from "../models/CatePro";
 import instance from "./Config";
 
-
 export const getAllCateProduct = (): Promise<ICatePro[]> => {
-    const url = "/cateproduct";
-    return instance.get(url);
-}
-
+  const url = "/cateproduct";
+  return instance.get(url);
+};
+export const getAllCateProductadmin = (): Promise<ICatePro[]> => {
+  const url = "/cateproducts";
+  return instance.get(url);
+};
 export const readCateProduct = (id: string): Promise<ICatePro> => {
-    const url = `/cateproduct/${id}`;
-    return instance.get(url);
-}
+  const url = `/cateproduct/${id}`;
+  return instance.get(url);
+};
 
 export const createCateProduct = (catepost: ICatePro): Promise<ICatePro> => {
-    const url = `/cateproduct`;
-    return instance.post(url, catepost);
-}
+  const url = `/cateproduct`;
+  return instance.post(url, catepost);
+};
 
 export const updateCateProduct = (catepost: ICatePro): Promise<ICatePro> => {
-    const url = `/cateproduct/${catepost._id}`;
-    return instance.put(url, catepost);
-}
+  const url = `/cateproduct/${catepost._id}`;
+  return instance.put(url, catepost);
+};
 
 export const removeCateProduct = (id: string): Promise<ICatePro> => {
-    const url = `/cateproduct/${id}`;
-    return instance.delete(url);
-}
+  const url = `/cateproduct/${id}`;
+  return instance.delete(url);
+};
